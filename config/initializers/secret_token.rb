f753ba6c396c6fac96e1ae8e8a57f30f1cfe737f
Geoff Walmsley <mathsboy@gmail.com>
2011-12-31 00:06:03 +1300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tew::Application.config.secret_token = 'c798efd6db20a278d348d78d25d41fa9589f1efef8cf384174787f0b9588c69e3353fb38c49b9c8b7121efe32d230585a6e33db3794f14f21b52c5d71f0ad592'
